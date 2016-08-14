19343148e585d29354db2b6baddbfe83e018f8b0
HsinWei <fatjuan2000@hotmail.com>
2016-08-14 12:17:25 +0800

    Para initial

--- app/src/main/java/tw/com/key214kimo/rotoolbox/Numerical.java
+++ app/src/main/java/tw/com/key214kimo/rotoolbox/Numerical.java
@@ -32,7 +32,19 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class Numerical extends AppCompatActivity {
-    Spinner spinner;
+    Spinner spinnerStr;
+    Spinner spinnerAgi;
+    Spinner spinnerVit;
+    Spinner spinnerIn;
+    Spinner spinnerDex;
+    Spinner spinnerLuk;
+    int StrPos;
+    int AgiPos;
+    int VitPos;
+    int InPos;
+    int DexPos;
+    int LukPos;
+    TextView tv16;
     Spinner spinner1;
     Spinner spinner2;
     Spinner spinner3;
@@ -100,97 +112,6 @@ public class Numerical extends AppCompatActivity {
             }
             //menuList[i].setGroupCheckable(GupID, true, true);
         }
-
-        /*
-        SubMenu menu1 = menu.addSubMenu(0, 0, Menu.NONE, "初學者");
-        SubMenu menu2 = menu.addSubMenu(0, 1, Menu.NONE, "劍　士");
-        SubMenu menu3 = menu.addSubMenu(0, 2, Menu.NONE, "魔法師");
-        SubMenu menu4 = menu.addSubMenu(0, 3, Menu.NONE, "商　人");
-        SubMenu menu5 = menu.addSubMenu(0, 4, Menu.NONE, "盜　賊");
-        SubMenu menu6 = menu.addSubMenu(0, 5, Menu.NONE, "服　事");
-        SubMenu menu7 = menu.addSubMenu(0, 6, Menu.NONE, "弓箭手");
-        SubMenu menu8 = menu.addSubMenu(0, 7, Menu.NONE, "跆拳家");
-        SubMenu menu9 = menu.addSubMenu(0, 8, Menu.NONE, "忍　者");
-        SubMenu menu10 = menu.addSubMenu(0, 9, Menu.NONE, "神槍手");
-
-        menu1.add(1, 0, Menu.NONE, "超級初學者");
-        menu1.add(1, 1, Menu.NONE, "超級初學者等級突破");
-        menu1.setGroupCheckable(1, true, true);
-
-        menu2.add(2, 0, Menu.NONE, "2-1職業");
-        menu2.add(2, 1, Menu.NONE, "騎　士");
-        menu2.add(2, 2, Menu.NONE, "騎士領主");
-        menu2.add(2, 3, Menu.NONE, "盧恩騎士");
-        menu2.add(2, 4, Menu.NONE, "2-2職業");
-        menu2.add(2, 5, Menu.NONE, "十字軍");
-        menu2.add(2, 6, Menu.NONE, "聖殿十字軍");
-        menu2.add(2, 7, Menu.NONE, "皇家禁衛隊");
-        menu2.setGroupCheckable(2, true, true);
-
-        menu3.add(3, 0, Menu.NONE, "2-1職業");
-        menu3.add(3, 1, Menu.NONE, "巫　師");
-        menu3.add(3, 2, Menu.NONE, "超魔導師");
-        menu3.add(3, 3, Menu.NONE, "咒術士");
-        menu3.add(3, 4, Menu.NONE, "2-2職業");
-        menu3.add(3, 5, Menu.NONE, "賢　者");
-        menu3.add(3, 6, Menu.NONE, "智　者");
-        menu3.add(3, 7, Menu.NONE, "妖術師");
-        menu3.setGroupCheckable(3, true, true);
-
-        menu4.add(4, 0, Menu.NONE, "2-1職業");
-        menu4.add(4, 1, Menu.NONE, "鐵　匠");
-        menu4.add(4, 2, Menu.NONE, "神工匠");
-        menu4.add(4, 3, Menu.NONE, "機械工匠");
-        menu4.add(4, 4, Menu.NONE, "2-2職業");
-        menu4.add(4, 5, Menu.NONE, "鍊金術師");
-        menu4.add(4, 6, Menu.NONE, "創造者");
-        menu4.add(4, 7, Menu.NONE, "基因學者");
-        menu4.setGroupCheckable(4, true, true);
-
-        menu5.add(5, 0, Menu.NONE, "2-1職業");
-        menu5.add(5, 1, Menu.NONE, "刺　客");
-        menu5.add(5, 2, Menu.NONE, "十字刺客");
-        menu5.add(5, 3, Menu.NONE, "十字斬首者");
-        menu5.add(5, 4, Menu.NONE, "2-2職業");
-        menu5.add(5, 5, Menu.NONE, "流　氓");
-        menu5.add(5, 6, Menu.NONE, "神行太保");
-        menu5.add(5, 7, Menu.NONE, "魅影追蹤者");
-        menu5.setGroupCheckable(5, true, true);
-
-        menu6.add(6, 0, Menu.NONE, "2-1職業");
-        menu6.add(6, 1, Menu.NONE, "祭　司");
-        menu6.add(6, 2, Menu.NONE, "神　官");
-        menu6.add(6, 3, Menu.NONE, "大主教");
-        menu6.add(6, 4, Menu.NONE, "2-2職業");
-        menu6.add(6, 5, Menu.NONE, "武道家");
-        menu6.add(6, 6, Menu.NONE, "武術宗師");
-        menu6.add(6, 7, Menu.NONE, "修　羅");
-        menu6.setGroupCheckable(6, true, true);
-
-        menu7.add(7, 0, Menu.NONE, "2-1職業");
-        menu7.add(7, 1, Menu.NONE, "獵　人");
-        menu7.add(7, 2, Menu.NONE, "神射手");
-        menu7.add(7, 3, Menu.NONE, "遊　俠");
-        menu7.add(7, 4, Menu.NONE, "2-2職業");
-        menu7.add(7, 5, Menu.NONE, "舞孃(女)");
-        menu7.add(7, 6, Menu.NONE, "冷豔舞姬(女)");
-        menu7.add(7, 7, Menu.NONE, "浪跡舞者(女)");
-        menu7.add(7, 8, Menu.NONE, "吟遊詩人(男)");
-        menu7.add(7, 9, Menu.NONE, "搞笑藝人(男)");
-        menu7.add(7, 10, Menu.NONE, "宮廷樂師(男)");
-        menu7.setGroupCheckable(7, true, true);
-
-        menu8.add(8, 0, Menu.NONE, "拳　聖");
-        menu8.add(8, 1, Menu.NONE, "悟靈士");
-        menu8.setGroupCheckable(8, true, true);
-
-        menu9.add(9, 0, Menu.NONE, "影狼(男)");
-        menu9.add(9, 1, Menu.NONE, "朧(女)");
-        menu9.setGroupCheckable(9, true, true);
-
-        menu10.add(10, 0, Menu.NONE, "反叛者");
-        menu10.setGroupCheckable(10, true, true);
-        */
         return true;
     }
 
@@ -199,67 +120,152 @@ public class Numerical extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numerical);
 
+        StrPos=1;
+        AgiPos=1;
+        VitPos=1;
+        InPos=1;
+        DexPos=1;
+        LukPos=1;
 
         String str[] = new String[130];
         for (int i = 0; i < str.length; i++) {
             str[i] = "" + (i + 1);
         }
-        spinner = (Spinner) findViewById(R.id.str);
+        spinnerStr = (Spinner) findViewById(R.id.str);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(Numerical.this,
                 android.R.layout.simple_spinner_dropdown_item,
                 str);
-        spinner.setAdapter(adapter);
+        spinnerStr.setAdapter(adapter);
+        spinnerStr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                StrPos = position+1;
+                Log.d("T0814-A","Str:"+StrPos);
+                ParaValue();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 /******************************************************/
         String agi[] = new String[130];
         for (int i = 0; i < agi.length; i++) {
             agi[i] = "" + (i + 1);
         }
-        spinner = (Spinner) findViewById(R.id.agi);
+        spinnerAgi = (Spinner) findViewById(R.id.agi);
         ArrayAdapter<String> adapteragi = new ArrayAdapter<String>(Numerical.this,
                 android.R.layout.simple_spinner_dropdown_item,
                 agi);
-        spinner.setAdapter(adapteragi);
+        spinnerAgi.setAdapter(adapteragi);
+        spinnerAgi.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                AgiPos = position+1;
+                Log.d("T0814-A","Agi:"+AgiPos);
+                ParaValue();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 /******************************************************/
         String vit[] = new String[130];
         for (int i = 0; i < vit.length; i++) {
             vit[i] = "" + (i + 1);
         }
-        spinner = (Spinner) findViewById(R.id.vit);
+        spinnerVit = (Spinner) findViewById(R.id.vit);
         ArrayAdapter<String> adapteragivit = new ArrayAdapter<String>(Numerical.this,
                 android.R.layout.simple_spinner_dropdown_item,
                 vit);
-        spinner.setAdapter(adapteragivit);
+        spinnerVit.setAdapter(adapteragivit);
+        spinnerVit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                VitPos = position+1;
+                Log.d("T0814-A","Vit:"+VitPos);
+                ParaValue();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 /******************************************************/
         String In[] = new String[130];
         for (int i = 0; i < agi.length; i++) {
             In[i] = "" + (i + 1);
         }
-        spinner = (Spinner) findViewById(R.id.in);
+        spinnerIn = (Spinner) findViewById(R.id.in);
         ArrayAdapter<String> adapteragiin = new ArrayAdapter<String>(Numerical.this,
                 android.R.layout.simple_spinner_dropdown_item,
                 In);
-        spinner.setAdapter(adapteragiin);
+        spinnerIn.setAdapter(adapteragiin);
+        spinnerIn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                InPos = position+1;
+                Log.d("T0814-A","In:"+InPos);
+                ParaValue();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 /******************************************************/
         String dex[] = new String[130];
         for (int i = 0; i < agi.length; i++) {
             dex[i] = "" + (i + 1);
         }
-        spinner = (Spinner) findViewById(R.id.dex);
+        spinnerDex = (Spinner) findViewById(R.id.dex);
         ArrayAdapter<String> adapteragidex = new ArrayAdapter<String>(Numerical.this,
                 android.R.layout.simple_spinner_dropdown_item,
                 dex);
-        spinner.setAdapter(adapteragidex);
+        spinnerDex.setAdapter(adapteragidex);
+        spinnerDex.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                DexPos = position+1;
+                Log.d("T0814-A","Dex:"+DexPos);
+                ParaValue();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 /******************************************************/
         String luk[] = new String[130];
         for (int i = 0; i < agi.length; i++) {
             luk[i] = "" + (i + 1);
         }
-        spinner = (Spinner) findViewById(R.id.luk);
+        spinnerLuk = (Spinner) findViewById(R.id.luk);
         ArrayAdapter<String> adapteragiluk = new ArrayAdapter<String>(Numerical.this,
                 android.R.layout.simple_spinner_dropdown_item,
                 luk);
-        spinner.setAdapter(adapteragiluk);
+        spinnerLuk.setAdapter(adapteragiluk);
+        spinnerLuk.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                LukPos = position+1;
+                Log.d("T0814-A","Luk:"+LukPos);
+                ParaValue();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 /******************************************************/
 
         String speed1[] = {"無效果", "瓜拿那糖果10%", "集中藥水10%", "覺醒藥水15%", "菠色藥藥水20%", "毒藥瓶20%(十次刺客/十字斬首者限定)"};
@@ -380,41 +386,40 @@ public class Numerical extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        Log.d("T0810-Smenu","Group:"+item.getGroupId());
-        //Log.d("T0810-Smenu","Item:"+item.getItemId());
-        Log.d("T0810-Smenu","Title:"+item.getTitle());
-        //if (item.getGroupId()==500 && item.getItemId()==500)
-
-        //ID="巫師";
+        Log.d("T8014-In",spinnerIn.getSelectedItem().toString());
+        //取得職業資料
         ROASlist list = new ROASlist(Numerical.this);
         ID= item.getTitle().toString();
 
+        //將所選的職業名稱丟入取出數值表
         list=list.SeacrhList(ID);
         //Log.d("T0809-list", list.ID + ",空手:" + list.Empty);
 
+        //秀出目前職業
         TextView tv31 = (TextView)findViewById(R.id.textView31);
+        //
         if(item.getGroupId()!=0)
         tv31.setText(ID);
         //tv.setText(ID);
 
+        //將可裝備武器資料 放到 Spinner
         ArrayList<ShowIDListArray> sid=list.ShowList(list);
-        str = new String[sid.size()];
+        str = new String[sid.size()];   //決定 顯示數量
         para1 = new float[sid.size()];
         HandUsed=new int[sid.size()];
         final float shd=Float.parseFloat(list.Shield);
+        //將資料放置同一 position
         for(int i=0; i< sid.size();i++)
         {
-            //Log.d("T0809-E",sid.get(i).Weapon+":"+sid.get(i).speed);
             str[i]=sid.get(i).Weapon;
             para1[i]=Float.parseFloat(sid.get(i).speed);
             HandUsed[i]=sid.get(i).HandUsed;
             Log.d("T0809-E1",str[i]+":"+para1[i]+":"+HandUsed[i]);
-            //sid.get(i).Weapon;
-            //sid.get(i).speed;
         }
-        Spinner sp1= (Spinner)findViewById(R.id.spinner);
+        final Spinner sp1= (Spinner)findViewById(R.id.spinner);
         ArrayAdapter<String> adpt = new ArrayAdapter<String>(Numerical.this,android.R.layout.simple_list_item_1,str);
         sp1.setAdapter(adpt);
+
         sp1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -427,15 +432,28 @@ public class Numerical extends AppCompatActivity {
                     cb.setEnabled(true);
                 }
 
-                if(cb.isChecked())
-                    Log.d("T0813-shd",""+shd);
+
                 cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                     @Override
                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        Log.d("T0813-shd",""+shd);
                         Log.d("T0813-shd",""+isChecked);
                     }
                 });
-                //tv42.setText(""+para1[i]);
+
+                Spinner sp1 = (Spinner)findViewById(R.id.spinner1);
+
+                sp1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        Log.d("T0813-buffS",""+position);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
             }
 
             @Override
@@ -445,5 +463,18 @@ public class Numerical extends AppCompatActivity {
         });
 
         return super.onOptionsItemSelected(item);
+    }
+    public void AttSpeed(int WPos,int BuffPos)
+    {
+
+    }
+
+    public void ParaValue()
+    {
+        int sum;
+        sum=StrPos+AgiPos+VitPos+InPos+DexPos+LukPos;
+        Log.d("T0814-A","sum:"+sum);
+        tv16=(TextView)findViewById(R.id.textView16);
+        tv16.setText(""+sum);
     }
 }
